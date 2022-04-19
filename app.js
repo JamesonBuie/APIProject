fe55d09ba104869679f764b2ad6cadd08fadc368
Jameson Buie <jamesonbuie@gmail.com>
2022-04-19 09:54:24 -0500

    Initialized API Project

---
+++ app.js
@@ -0,0 +1,13 @@
+// async function getSportsData() {
+   
+//     try {
+//        const dataPointOne = await axios.get(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=MLB`);
+//        console.log(`Get request was successful`);
+//        console.log(dataPointOne.data.teams[22]);
+//        console.log(`=======`);
+//     } catch(err){
+//         console.log(`Get request was not successful`);
+//         console.log(err);
+//     }
+// }
+// getSportsData();
