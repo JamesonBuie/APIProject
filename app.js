04fb97756187745d6ddb90e8a2c8cd80f25ffd1a
Jameson Buie <jamesonbuie@gmail.com>
2022-04-20 13:47:48 -0500

    Completed grid that data will be displayed on. Modified html fuile

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-// async function getSportsData() {
+/* 
+Data names
+Logo = strTeamLogo
+Description = strDescriptionEN
+Year formed = intFormedYear
+Stadium photo = strStadiumThumb
+Jersey = strTeamJersey
+
+*/
+
+
+
+
+async function getSportsData() {
    
-//     try {
-//        const dataPointOne = await axios.get(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=MLB`);
-//        console.log(`Get request was successful`);
-//        console.log(dataPointOne.data.teams[22]);
-//        console.log(`=======`);
-//     } catch(err){
-//         console.log(`Get request was not successful`);
-//         console.log(err);
-//     }
-// }
-// getSportsData();
+    try {
+       const dataPointOne = await axios.get(`https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=MLB`);
+       console.log(`Get request was successful`);
+       console.log(dataPointOne.data.teams);
+       console.log(`=======`);
+    } catch(err){
+        console.log(`Get request was not successful`);
+        console.log(err);
+    }
+}
+getSportsData();
+
